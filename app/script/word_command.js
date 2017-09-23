55ac71cf134e198782a27c5e71d5366f1e63cb7f
ichi <30754019+ichizero@users.noreply.github.com>
2017-09-23 15:44:34 +0900

    to work on docker

---
+++ app/script/word_command.js
@@ -0,0 +1,10 @@
+function wordMatch(str) {
+    var birthday = "誕生日";
+    if(string.indexOf(birthday) > -1){
+        happyBirthday();
+    }
+}
+
+function happyBirthday() {
+    createFirework(52,69,7,6,null,null,null,null,false,true);
+}
